4e513391219e7a3d820c52cfb7d24855334bf130
PriyankaG96 <70764254+PriyankaG96@users.noreply.github.com>
2022-04-12 14:51:27 +0530

    Question 7

---
+++ model.py
@@ -0,0 +1,56 @@
+import string
+from flask import Flask, request
+from flasgger import Swagger  ###
+import pickle
+from sklearn.linear_model import LogisticRegression
+
+app = Flask(__name__)      ###constreuctor
+Swagger(app)
+
+pickled_model_file = open("pickle_iris_model.pkl","rb")
+classifier = pickle.load(pickled_model_file)
+
+@app.route('/')
+def home():
+    return "Welcome to Iris Classifier"
+
+@app.route("/predict")
+def perdict_flower():
+    
+    """Lets try Swagger fromFlasgger
+    ---
+    parameters:
+        - name : Sepal_width
+          in : query
+          type : number
+          required : true
+        - name : Sepal_length
+          in : query
+          type : number
+          required : true
+        - name : Petal_width
+          in : query
+          type : number
+          required : true
+        - name : Petal_length
+          in : query
+          type : number
+          required : true
+    responses:
+        200:
+            description : The result is
+    """
+          
+    sw = request.args.get("Sepal_width")
+    sl = request.args.get("Sepal_length")
+    pw = request.args.get("Petal_width")
+    pl = request.args.get("Petal_length")
+
+    result = classifier.predict([[sw,sl,pw,pl]])
+
+
+    return f"The prediction is that the flower belongs to class {result}"
+
+if __name__ == "__main__":
+    #app.run(host = "0.0.0.0", port = 8000,debug=True)
+    app.run(debug=True)
